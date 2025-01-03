f0d84dcf41fb3d7bacd5555109361833353c5d3c
Thai An <nguyenthaian210506@gmail.com>
2025-01-03 18:13:43 +0700

    bai2/buoi9

--- buoi9/b2.c
+++ buoi9/b2.c
@@ -1,2 +1,13 @@
-#include<stdio.h>
-#
+#include <stdio.h>
+int checkEven();
+int main()
+{
+    printf("\n%d", checkEven());
+    return 0;
+}
+
+int checkEven(){
+    int n;
+    scanf("%d", &n);
+    return (n%2==0)*1;
+}
