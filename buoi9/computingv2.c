c01f02dfdc2686a2b039c79ad82de3560b700db6
Thai An <nguyenthaian210506@gmail.com>
2025-01-05 11:46:20 +0700

    update1

--- buoi9/computingv2.c
+++ buoi9/computingv2.c
@@ -6,61 +6,76 @@ int order(char ch);
 int tinh(int a, int b, char ch);
 
 int main(){
-    char bieu_thuc[1000];
-    printf("Nhap bieu thuc di ban: ");
-    scanf("%s", bieu_thuc);
+    char again = 'Y';
+    printf("\033[32m===========================\033[0m\n");
+    printf("Chuong Trinh Tinh Bieu Thuc\n");
+    printf("\033[32m===========================\033[0m\n");
+    while(again == 'Y' || again == 'y'){
+        char bieu_thuc[1000] ={0};
+        printf("\n>> Nhap bieu thuc di ban [vd: 3+5*(7-2)]:  ");
+        fflush(stdin);
+        scanf("%s", bieu_thuc);
 
-    int stack_giatri[100];
-    char stack_op[100];                             // op: operator(+-*/ vaf () )
-    int first_giatri = -1, first_op = -1;           // vị rí đầu trong stack là -1 nghĩa là k có gì
+        int stack_giatri[100];
+        char stack_op[100];                             // op: operator(+-*/ vaf () )
+        int first_giatri = -1, first_op = -1;           // vị rí đầu trong stack là -1 nghĩa là k có gì
 
-    for(int i = 0; bieu_thuc[i] != 0; i++){
-        if(checkDigit(bieu_thuc[i])){
-            int temp = 0;                            // tam thoi luu so do có thể số lon hon 1 digit
-            while(checkDigit(bieu_thuc[i]) && bieu_thuc[i] != 0){       /* luu y */
-                temp = temp * 10 + (bieu_thuc[i] - 48);                  // -48 sẽ trở về đúng số đó vì '0' là 48
-                i++;
-            }                
-            i--;                                    // đk thoát là khác số nên lúc đó i khác số, i lùi lại để vòng sau kt tiếp
-            first_giatri++;
-            stack_giatri[first_giatri] = temp;      // di chuyển index lên và thêm 1 số vào stack                                             
-        }else if(bieu_thuc[i] == '('){
-            first_op++;
-            stack_op[first_op] = '(';    
-        }else if(checkOperator(bieu_thuc[i])){
-            while(first_op >= 0 && order(bieu_thuc[i]) <= order(stack_op[first_op])){
-                int a = stack_giatri[first_giatri];
-                first_giatri--;
-                int b = stack_giatri[first_giatri];
-                char op = stack_op[first_op];
-                first_op--;
-                stack_giatri[first_giatri] = tinh(a, b, op);
+        for(int i = 0; bieu_thuc[i] != 0; i++){
+            if(checkDigit(bieu_thuc[i])){
+                int temp = 0;                            // tam thoi luu so do có thể số lon hon 1 digit
+                while(checkDigit(bieu_thuc[i]) && bieu_thuc[i] != 0){       /* luu y */
+                    temp = temp * 10 + (bieu_thuc[i] - 48);                  // -48 sẽ trở về đúng số đó vì '0' là 48
+                    i++;
+                }                
+                i--;                                    // đk thoát là khác số nên lúc đó i khác số, i lùi lại để vòng sau kt tiếp
+                first_giatri++;
+                stack_giatri[first_giatri] = temp;      // di chuyển index lên và thêm 1 số vào stack                                             
+            }else if(bieu_thuc[i] == '('){
+                first_op++;
+                stack_op[first_op] = '(';    
+            }else if(checkOperator(bieu_thuc[i])){
+                while(first_op >= 0 && order(bieu_thuc[i]) <= order(stack_op[first_op])){
+                    int a = stack_giatri[first_giatri];
+                    first_giatri--;
+                    int b = stack_giatri[first_giatri];
+                    char op = stack_op[first_op];
+                    first_op--;
+                    stack_giatri[first_giatri] = tinh(a, b, op);
+                }
+                first_op++;
+                stack_op[first_op] = bieu_thuc[i];          // Ko vào while để tính thì +1 vào stack op
+            }else if( bieu_thuc[i] == ')'){                 // Tính heetts trong ngoặc và 100% ko còn ngoặc bên trong và đc xép theo */ trước
+                while(stack_op[first_op] != '(' && first_op >= 0){     // first_op >= 0 vi can co sack de tinh
+                    int a = stack_giatri[first_giatri];
+                    first_giatri--;
+                    int b = stack_giatri[first_giatri];
+                    char op = stack_op[first_op];
+                    first_op--;
+                    stack_giatri[first_giatri] = tinh(a, b, op);
+                }
+                first_op--;                                             // bo qua dau (
+            }else{
+                printf("\n\033[31m\nBan nhap sai forma!!\n\033[31m");
+                goto reTry;
             }
-            first_op++;
-            stack_op[first_op] = bieu_thuc[i];          // Ko vào while để tính thì +1 vào stack op
-        }else if( bieu_thuc[i] == ')'){                 // Tính heetts trong ngoặc và 100% ko còn ngoặc bên trong và đc xép theo */ trước
-            while(stack_op[first_op] != '(' && first_op >= 0){     // first_op >= 0 vi can co sack de tinh
-                int a = stack_giatri[first_giatri];
-                first_giatri--;
-                int b = stack_giatri[first_giatri];
-                char op = stack_op[first_op];
-                first_op--;
-                stack_giatri[first_giatri] = tinh(a, b, op);
-            }
-            first_op--;                                             // bo qua dau (
+
         }
-
+        // xu li phan con lai cua stack sau khi duyet het  bieu thuc
+        while( first_op >= 0){
+            int a = stack_giatri[first_giatri];
+            first_giatri--;
+            int b = stack_giatri[first_giatri];
+            char op = stack_op[first_op];
+            first_op--;
+            stack_giatri[first_giatri] = tinh(a, b, op);
+        }
+        printf("\n\033[32m>> Ket qua: %d\033[32m", stack_giatri[first_giatri]);
+        reTry:printf("\033[33m\nBan muon choi nua hong?? [Y/N] ");
+        fflush(stdin);
+        fflush(stdout);
+        scanf("%c", &again);
     }
-    // xu li phan con lai cua stack sau khi duyet het  bieu thuc
-    while( first_op >= 0){
-        int a = stack_giatri[first_giatri];
-        first_giatri--;
-        int b = stack_giatri[first_giatri];
-        char op = stack_op[first_op];
-        first_op--;
-        stack_giatri[first_giatri] = tinh(a, b, op);
-    }
-    printf("\n%d", stack_giatri[first_giatri]);
+    printf("\n\033[32m\nCam on nhieu nhieu\n\033[32m");
     return 0;
 }
 
